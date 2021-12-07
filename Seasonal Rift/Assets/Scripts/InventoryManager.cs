88a8275318af836ca390faae2278ea007010db21
Matt Izzo <46375091+Mattyiz@users.noreply.github.com>
2021-12-07 16:54:49 -0500

    Den Created
    Code wise that is

--- Seasonal Rift/Assets/Scripts/InventoryManager.cs
+++ Seasonal Rift/Assets/Scripts/InventoryManager.cs
@@ -5,11 +5,14 @@ using UnityEngine;
 public class InventoryManager : MonoBehaviour
 {
     public static List<ItemObject> items = new List<ItemObject>();
+    public static int stickCount;
+    public static bool denCreated;
 
     // Start is called before the first frame update
     void Start()
     {
-        
+        stickCount = 0;
+        denCreated = false;
     }
 
     // Update is called once per frame
@@ -23,6 +26,39 @@ public class InventoryManager : MonoBehaviour
             {
                 Debug.Log(items[i].type);
             }
+            if(denCreated)
+            {
+                Debug.Log("den");
+            }
         }
+
+        if(!denCreated)
+        {
+            for (int i = 0; i < items.Count; i++)
+            {
+                if (items[i].type == "stick")
+                {
+                    stickCount++;
+                }
+            }
+
+            if (stickCount == 5)
+            {
+                denCreated = true;
+                Debug.Log("Den Created");
+
+                for (int i = 0; i < items.Count; i++)
+                {
+                    if (items[i].type == "stick")
+                    {
+                        items.RemoveAt(i);
+                        i--;
+                    }
+                }
+            }
+            stickCount = 0;
+        }
+
+
     }
 }
--- Seasonal Rift/Assets/Scripts/ItemObject.cs
+++ Seasonal Rift/Assets/Scripts/ItemObject.cs
@@ -11,7 +11,7 @@ public class ItemObject : InteractiveParent
     // Start is called before the first frame update
     void Start()
     {
-        type = "Box 1";
+
     }
 
     // Update is called once per frame
